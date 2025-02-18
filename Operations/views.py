83e4fb7858ac220f595d7982b51ea751117382b8
Josephat <sangwajozaphat@gmail.com>
2025-02-18 21:08:35 +0200

    Added comments for more clarification

--- Operations/views.py
+++ Operations/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+# Creating a method for calling index page
 def home(request):
     return render(request,'index.html')
-
+# function for checking the operator and display the result
 def operation(request):
+# Fetching variables
     val1 = int(request.GET["num1"])
     val2 = int(request.GET["num2"])
-
+# Fetching the operator
     operator = request.GET["operator"]
-
+# Checking the operator and performing the operation
     if(operator == '+'):
         res = val1 + val2
         return render(request,'index.html',{'result': res})
